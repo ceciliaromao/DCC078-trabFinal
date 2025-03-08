158c533f10441ed5ea370ef5edf7499e3d352d27
Cecília Romão <ceciliaromao@gmail.com>
2025-03-08 03:31:41 -0300

    remove mint and redeem

--- src/main/java/templatemethod/RedeemBlockProcessor.java
+++
@@ -1,8 +0,0 @@
-package templatemethod;
-
-public class RedeemBlockProcessor extends BlockProcessor {
-    @Override
-    protected String validateBlock(String data) {
-        return "Validating Redeem Block: " + data;
-    }
-}
